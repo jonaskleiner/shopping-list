d7962a8ef94798fc8423fb5c48d62d7303bb3939
Jonas Kleiner <kleinerdesign@gmail.com>
2015-09-24 14:11:46 -0700

    sucessfully add items via form in test phase

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
-$(document).ready(function() {
-	
-	$( "body" ).on( "click", "p", function() {
-  		$( this ).after( "<p>I am a paragraph!</p>");
-	});
-});
-
-function displayVals() {
-  var singleValues = $( "#single" ).val();
-  var multipleValues = $( "#multiple" ).val() || [];
-  $( "p" ).html( "<b>Single:</b> " + singleValues +
-    " <b>Multiple:</b> " + multipleValues.join( ", " ) );
+function addListItem () {
+	// grab the value of the input field 'new=item'
+	// assign the value to a var called 'item'
+	var item = $("#new-item").val();
+	// find id "theList" and drop the var data into it
+	$("#theList").append('<li>'+item+'</li>');
+	// clear the input field
+	$("#new-item").val('');
 }
+
+$(document).ready(function() {
+	// input items into list
+	// on click run the function addListItem
+	$("#add").on('click', addListItem);
+});
